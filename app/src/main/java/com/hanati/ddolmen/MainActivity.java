0ce3150ee65cf949fc8cbe90dc3ecd935c93520c
HwangDongJoo <hdj1991@naver.com>
2018-01-04 20:59:12 +0900

    1차 커밋

--- app/src/main/java/com/hanati/ddolmen/MainActivity.java
+++ app/src/main/java/com/hanati/ddolmen/MainActivity.java
@@ -1,13 +1,35 @@
 package com.hanati.ddolmen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_history, btn_counsel, btn_join, btn_hanamembers, btn_hanalife;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+
     }
+
+    void initView(){    //화면의 View 정의
+        btn_history = (Button)findViewById(R.id.btn_history);
+        btn_counsel = (Button)findViewById(R.id.btn_counsel);
+        btn_join = (Button)findViewById(R.id.btn_join);
+        btn_hanamembers = (Button)findViewById(R.id.btn_hanamembers);
+        btn_hanalife = (Button)findViewById(R.id.btn_hanalife);
+
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+    }
+
+
 }
+
