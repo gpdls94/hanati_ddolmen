5c9b431e7c1e7d668d16de02be70e6e975e72100
혜인 <hyeinee1@naver.com>
2018-01-04 23:56:42 +0900

    no message

--- app/src/main/java/com/hanati/ddolmen/SplashActivity.java
+++ app/src/main/java/com/hanati/ddolmen/SplashActivity.java
@@ -21,7 +21,7 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         };
-        handler.postDelayed(runnable, 1500);
+        handler.postDelayed(runnable, 2000);
 
     }
 }
